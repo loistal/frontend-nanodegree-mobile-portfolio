d3898696303a1b01991e748d3768fca3e4e4af25
Lois Talagrand <lois.talagrand@epfl.ch>
2017-03-16 09:24:03 -1000

    Use optimized images from insight

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,18 +12,19 @@ module.exports = function(grunt) {
                 tasks:  ['concat'],
             }
         },
-        imagemin: { 
-            all: { 
-                options: { 
+        imagemin: {
+            all: {
+                options: {
                     optimizationLevel: 7
                 },
                 files: [{
-                	expand: true, 
+                    expand: true,
                     src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
                     dest: 'dist/' // Destination path prefix
                 }]
             }
-        }
+        },
+        
     });
 
     grunt.loadNpmTasks('grunt-contrib-imagemin');
